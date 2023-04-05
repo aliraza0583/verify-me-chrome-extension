512359ebd415b816ec16fca9f13306b0220d6de4
ninja1xyz <129714805+ninja1xyz@users.noreply.github.com>
2023-04-05 16:25:17 +0500

    remove comments

--- src/utils/chromeService.ts
+++ src/utils/chromeService.ts
@@ -1,17 +1,12 @@
 export const storeData = (data: Object) => {
   localStorage.setItem("secret", JSON.stringify(data));
-  // chrome.storage.local.set({ secret: data });
 };
 
 export const getData = () => {
   return localStorage.getItem("secret") || "";
-  // return chrome.storage.local.get("secret", (data) => {
-  //   console.log("data secret", data);
-  //   return data;
-  // });
+
 };
 
 export const RemoveData = () => {
   localStorage.removeItem("secret");
-  // chrome.storage.local.remove("myData");
 };
